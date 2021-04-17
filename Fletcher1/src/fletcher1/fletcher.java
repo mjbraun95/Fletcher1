34a23cee236103a09f099699d21e676830f090de
Matthew Braun <mjbraun@ualberta.ca>
2021-04-17 03:41:07 -0600

    Working well

--- Fletcher1/src/fletcher1/fletcher.java
+++ Fletcher1/src/fletcher1/fletcher.java
@@ -1,37 +1,73 @@
 package fletcher1;
 
-import java.awt.Graphics;
+//import java.awt.Graphics;
+import java.awt.Graphics2D;
 
+import org.osbot.rs07.api.Bank;
 import org.osbot.rs07.api.Inventory;
-import org.osbot.rs07.script.*;
+import org.osbot.rs07.api.model.RS2Object;
+import org.osbot.rs07.script.Script;
 import org.osbot.rs07.script.ScriptManifest;
 
-@ScriptManifest(author = "mjbraun95", info = "fletches bows", logo = "", name = "fletcher1", version = 0)
+@ScriptManifest(author = "mjbraun95", info = "fletches bows", logo = "", name = "fletcher1", version = 1.0)
 public class fletcher extends Script {
-	
-	// Code executed at beginning
-	public void onStart() {
-		getInventory();
-		
+	public Inventory inv;
+	public String logType;
+	public String bowType;
+	public RS2Object banker;
+	public Bank bank;
+	public void getInv() {
+		inv = getInventory();
 	}
 	
-	public int onLoop() {
+	@Override
+	public void onStart() {
 		
-		if(!getInventory().isFull()) {
-			
+		log("started!");
+		banker = getObjects().closest("Banker");
+		if (banker != null) {
+		  log("banker is present!");
+		  bank.getBank();
+		}
+		else {
+			log("banker is not present. Exiting");
+			System.exit(1);
+		}
+	}
+	
+	@Override
+	public int onLoop() throws InterruptedException {
+//		banker.interact("Bank");
+		bank.open();
+		bank.depositAll();
+		bank.withdraw("Knife", 1);
+		bank.withdraw(logType, 27);
+		getInv();
+		if (!inv.contains(logType) || !inv.contains("Knife")) {
+			log("did not successfully withdraw. Exiting");
+			System.exit(1);
+		}
+		else {
+			log("Withdrew items successfully!");
+		}
+		if(!inv.isFull()) {
+			log("Inventory full");
+		}
+		else {
+			log("Inventory not full");
 		}
 		
 		//Returns ms delay
-		return 50;
+		return 4000;
 	}
 	
-	// Code executed at end
+	@Override
 	public void onExit() {
 		
 	}
 	
-	// paint
-	public void onPaint(Graphics g) {
-		
+	@Override
+	public void onPaint(Graphics2D g) {
+		g.drawString("Hello World", 50, 50);
 	}
 }
